7db6a515214f58e298f1b37753f49ad3c4141acd
Gilson Chariguaman <gilson258@live.com>
2019-12-19 04:45:13 -0500

    mi app3

--- app/src/main/java/com/example/menusensores_gl/SensorAccelerometer.java
+++ app/src/main/java/com/example/menusensores_gl/SensorAccelerometer.java
@@ -1,19 +1,144 @@
 package com.example.menusensores_gl;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SensorAccelerometer extends Fragment {
+import com.example.menusensores_gl.accelerometer.GraphView;
+
+import static android.content.Context.SENSOR_SERVICE;
+
+public class SensorAccelerometer extends Fragment implements SensorEventListener {
+
+    private final static String TAG = "MainActivity";
+
+    private TextView rateView, accuracyView;
+    private GraphView xView, yView, zView;
+
+    private SensorManager sensorMgr;
+    private Sensor accelerometer;
+
+    private final static long GRAPH_REFRESH_WAIT_MS = 20;
+
+    private GraphRefreshThread th = null;
+    private Handler handler;
+
+    private float vx, vy, vz;
+    private float rate;
+    private int accuracy;
+    private long prevts;
+    private final int N = 5;
+    private float[] ax = new float[N];
+    private float[] ay = new float[N];
+    private float[] az = new float[N];
+    private int idx = 0;
+    private int idy = 0;
+    private int idz = 0;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sensor_accelerometer, container, false);
+
+      //  rateView = (TextView) view.findViewById(R.id.rate_view);
+       // accuracyView = (TextView) view.findViewById(R.id.accuracy_view);
+        xView = (GraphView) view.findViewById(R.id.x_view);
+        yView = (GraphView) view.findViewById(R.id.y_view);
+        zView = (GraphView) view.findViewById(R.id.z_view);
+
+        sensorMgr = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        handler = new Handler();
+
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+        sensorMgr.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        th = new GraphRefreshThread();
+        th.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+        th = null;
+        sensorMgr.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        ax[idx] = event.values[0];
+        float sx =0;
+        for(int i=0; i<N; i++){
+            sx = sx + ax[i];
+            vx = sx/N;
+            idx = (idx+1)%N;
+        }
+
+        ay[idy] = event.values[1];
+        float sy =0;
+        for(int i=0; i<N; i++){
+            sy = sy + ay[i];
+            vy = sy/N;
+            idy = (idy+1)%N;
+        }
+
+        az[idz] = event.values[2];
+        float sz =0;
+        for(int i=0; i<N; i++){
+            sz = sz + az[i];
+            vz = sz/N;
+            idz = (idz+1)%N;
+        }
+        rate = ((float) (event.timestamp - prevts)) / (1000 * 1000);
+        prevts = event.timestamp;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.i(TAG, "onAccuracyChanged: ");
+        this.accuracy = accuracy;
+    }
+
+    private class GraphRefreshThread extends Thread {
+        public void run() {
+            try {
+                while (th != null) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                           // rateView.setText(Float.toString(rate));
+                            //accuracyView.setText(Integer.toString(accuracy));
+                            xView.addData(vx, true);
+                            yView.addData(vy, true);
+                            zView.addData(vz, true);
+                        }
+                    });
+                    Thread.sleep(GRAPH_REFRESH_WAIT_MS);
+                }
+            }
+            catch (InterruptedException e) {
+                Log.e(TAG, e.toString());
+                th = null;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/menusensores_gl/accelerometer/GraphView.java
@@ -0,0 +1,94 @@
+package com.example.menusensores_gl.accelerometer;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import com.example.menusensores_gl.R;
+
+public class GraphView extends View {
+
+    private final static String TAG = "GraphView";
+    private final static float Ymax = 20;
+    private final static int NDATA_INIT = 256;
+
+    private int ndata = NDATA_INIT;
+    private float[] vs = new float[NDATA_INIT];
+    private int idx = 0;
+    private int x0, y0, ewidth;
+    private int dw = 5, dh = 1;
+
+    private final Paint paint = new Paint();
+
+    public GraphView(Context context) {
+        this(context, null);
+    }
+
+    public GraphView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GraphView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Log.i(TAG, "onSizeChanged: w=" + w + " h=" + h);
+        ndata = w / dw;
+        x0 = (w - dw * ndata) / 2;
+        y0 = h / 2;
+        ewidth = x0 + dw * (ndata - 1);
+
+        if (y0 / Ymax >= dh + 1)
+            dh = (int) (y0 / Ymax);
+        if (ndata > vs.length) {
+            idx = 0;
+            vs = new float[ndata];
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // grid lines
+        paint.setColor(Color.argb(70, 0, 0, 0));
+        paint.setStrokeWidth(5);
+        int h = canvas.getHeight();
+        for (int y = y0; y < h; y += dh * 5)
+            canvas.drawLine(x0, y, ewidth, y, paint);
+        for (int y = y0; y > 0; y -= dh * 5)
+            canvas.drawLine(x0, y, ewidth, y, paint);
+        for (int x = x0; x < dw * ndata; x += dw * 5)
+            canvas.drawLine(x, 0, x, h, paint);
+
+        // y0 line
+        paint.setColor(Color.argb(155,0,145,234));
+        canvas.drawLine(0, y0, ewidth, y0, paint);
+
+        // graph
+        paint.setColor(Color.argb(155,213,0,0));
+        paint.setStrokeWidth(7);
+        for (int i = 0; i < ndata - 1; i++) {
+            int j = (idx + i) % ndata;
+            int x1 = x0 + dw * i;
+            int x2 = x0 + dw * (i + 1);
+            int y1 = (int) (y0 + dh * vs[j]);
+            int y2 = (int) (y0 + dh * vs[(j + 1) % ndata]);
+            canvas.drawLine(x1, y1, x2, y2, paint);
+        }
+    }
+
+    public void addData(float val, boolean invalidate) {
+        vs[idx] = val;
+        idx = (idx + 1) % ndata;
+        if (invalidate)
+            invalidate();
+    }
+}
